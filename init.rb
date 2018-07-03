0a3559006d735b72f56a5392bca7e5c68e1e3fbc
[willem] <[willem@creativesounds.nl]>
2018-07-03 20:19:40 +0200

    (fix): moved hashbang to the first line of the program.

--- init.rb
+++ init.rb
@@ -1,5 +1,21 @@
 #!/usr/bin/env ruby
 
+# wav2mp3-watcher is a program that watches a folder for wav files and converts them to mp3 files.
+# Copyright (C) 2018  Willem Deen
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 require 'logger'
 require 'shellwords'
 require_relative('lib/scanner')
@@ -14,7 +30,7 @@ logger.level = Logger::INFO
 s = Scanner.new(path: folder_path)
 
 logger.info("###########################################################")
-logger.info("wav2mp3_watcher Started!".center(60))
+logger.info("wav2mp3-watcher Started!".center(60))
 logger.info("###########################################################")
 
 command = "/usr/local/bin/ffmpeg"
@@ -22,7 +38,6 @@ input_option = " -i "
 format_option = " -f mp3 "
 
 while true do
-
   s.items.each do |i|
     file_no_ext = Shellwords.shellescape i[0..-5]
     if system( "#{command}#{input_option}#{file_no_ext}.wav#{format_option}#{file_no_ext}.mp3" )
